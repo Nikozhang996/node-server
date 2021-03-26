43362d0b6ff51b744dc3f436bdd3340c41d003f8
zhangjianke <zhangjianke@wps.cn>
2021-03-26 11:23:16 +0800

    feat(all): 补充koa-redis
    新增koa-redis和koa-bodyparser中间件，添加set get方法

--- src/modules/koa/api/base/index.ts
+++ src/modules/koa/api/base/index.ts
@@ -1,4 +1,49 @@
-// import router from "koa-router";
+import Router from "koa-router";
+import redis from "redis";
 
-// export default router;
-const a = 1;
+const router = new Router();
+const redisClient = redis.createClient();
+
+router.get("/get-key", async function (ctx) {
+  const { name } = ctx.query;
+  if (typeof name === "string") {
+    console.log(name);
+    redisClient.get(name, function (err, reply) {
+      if (!err) {
+        console.log(reply);
+        ctx.body = reply;
+      }
+    });
+  } else {
+    ctx.body = "资源不存在";
+  }
+});
+router.post("/set-key", async function (ctx, next) {
+  const { key, value } = ctx.request.body;
+
+  redisClient.set(key, value, function (err, reply) {
+    if (!err) {
+      ctx.body = { code: 200, message: reply };
+      next();
+    }
+  });
+  ctx.body = { code: 200, message: 'success' };
+});
+router.get("/show-list", async function (ctx) {
+  ctx.body = {};
+});
+
+router.get("/test", async function (ctx) {
+  console.log(`${ctx.method} - ${ctx.url}`);
+
+  ctx.body = {
+    name: "zjk",
+    age: 20,
+  };
+});
+
+redisClient.on("error", function (error) {
+  console.error(error);
+});
+
+export default router;
--- src/modules/koa/index.ts
+++ src/modules/koa/index.ts
@@ -1,35 +1,21 @@
 import Koa from "koa";
-import Router from "koa-router";
 import logger from "koa-logger";
-import fs from "fs";
+import bodyParser from "koa-bodyparser";
 
+// router
+import router from "./api/base";
+
+const app = new Koa();
 const onerror = require("koa-onerror");
 
 const CONFIG = {
   port: 1995,
 };
 
-const router = new Router();
-const app = new Koa();
-
-app.use(router.routes());
 app.use(logger());
 onerror(app);
-
-
-router.get("/test", async function (ctx) {
-  console.log(`${ctx.method} - ${ctx.url}`);
-  fs.readFile("./api/base/index.ts", function (err, data) {
-    if (!err) {
-      ctx.body = data;
-    }
-  });
-
-  /*ctx.body = {
-    name: "zjk",
-    age: 20,
-  };*/
-});
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.on("error", (err, ctx) => {
   console.error("server error !!!!!!!!!!!!!", err, ctx);
